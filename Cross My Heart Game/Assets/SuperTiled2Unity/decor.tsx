318db81e3192e347a280e94e666021e04c11c186
vnck <ivanckj96@gmail.com>
2020-08-02 20:27:59 +0800

    added new tilesets

--- Cross My Heart Game/Assets/SuperTiled2Unity/decor.tsx
+++ Cross My Heart Game/Assets/SuperTiled2Unity/decor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="decor" tilewidth="16" tileheight="16" tilecount="280" columns="20">
- <image source="../Art/decor.png" width="320" height="230"/>
+<tileset version="1.2" tiledversion="1.3.5" name="decor" tilewidth="16" tileheight="16" tilecount="560" columns="20">
+ <image source="../Art/decor.png" width="320" height="460"/>
 </tileset>
--- Cross My Heart Game/Assets/SuperTiled2Unity/objects.tsx
+++ Cross My Heart Game/Assets/SuperTiled2Unity/objects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="objects" tilewidth="16" tileheight="16" tilecount="400" columns="20">
- <image source="../Art/objects.png" width="320" height="320"/>
+<tileset version="1.2" tiledversion="1.3.5" name="objects" tilewidth="16" tileheight="16" tilecount="800" columns="20">
+ <image source="../Art/objects.png" width="320" height="640"/>
 </tileset>
