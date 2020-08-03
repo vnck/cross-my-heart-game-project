6a3e393a45ac56901858578d41be532796d84383
vnck <ivanckj96@gmail.com>
2020-08-04 00:18:01 +0800

    fixes to tile layers, added new painting, added colours

---
+++ Cross My Heart Game/Assets/SuperTiled2Unity/Paintings.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Paintings" tilewidth="16" tileheight="16" tilecount="162" columns="9">
+ <image source="../Art/Paintings.png" width="144" height="288"/>
+</tileset>
