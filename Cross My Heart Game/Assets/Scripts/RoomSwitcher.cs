72eb05e68a6171ea388793acf57c5bd34c97c879
Eda <edathr2@gmail.com>
2020-08-09 14:30:33 +0800

    Made camera and light immediately shift in RoomSwitcher

--- Cross My Heart Game/Assets/Scripts/RoomSwitcher.cs
+++ Cross My Heart Game/Assets/Scripts/RoomSwitcher.cs
@@ -8,6 +8,8 @@ public class RoomSwitcher : MonoBehaviour
     public int destinationSwitchId;
     public GameObject destinationSwitch;
     static GameObject player;
+    static GameObject playerLight;
+    static GameObject camera;
     public float destinationOffset;
     public bool activated;
     
@@ -15,6 +17,8 @@ public class RoomSwitcher : MonoBehaviour
     void Start()
     {
         if (player == null) { player = GameObject.FindGameObjectWithTag("Player"); }
+        if (playerLight == null) { playerLight = GameObject.FindGameObjectWithTag("PlayerLight"); }
+        if (camera == null) { camera = GameObject.FindGameObjectWithTag("MainCamera"); }
     }
 
     // Update is called once per frame
@@ -24,11 +28,15 @@ public class RoomSwitcher : MonoBehaviour
                 if (GetComponent<Door>().isLocked == false) {
                     Debug.Log("Room Switch " + switchId + " triggered, going to Room Switch " + destinationSwitchId);
                     player.transform.position = new Vector3(destinationSwitch.transform.position.x, destinationSwitch.transform.position.y + destinationOffset, 0);
+                    playerLight.transform.position = new Vector3(destinationSwitch.transform.position.x, destinationSwitch.transform.position.y + destinationOffset, 0);
+                    camera.transform.position = new Vector3(destinationSwitch.transform.position.x, destinationSwitch.transform.position.y + destinationOffset, -10);
                 }
             } else if (GetComponent<Door>() == null) {
                 Debug.Log(destinationOffset);
                 Debug.Log("Room Switch " + switchId + " triggered, going to Room Switch " + destinationSwitchId);
                 player.transform.position = new Vector3(destinationSwitch.transform.position.x, destinationSwitch.transform.position.y + destinationOffset, 0);
+                playerLight.transform.position = new Vector3(destinationSwitch.transform.position.x, destinationSwitch.transform.position.y + destinationOffset, 0);
+                camera.transform.position = new Vector3(destinationSwitch.transform.position.x, destinationSwitch.transform.position.y + destinationOffset, -10);
             }
         }
     }
